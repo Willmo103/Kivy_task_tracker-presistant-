142301707af240685af42067bc09669318d73d92
Willmo103 <88165849+Willmo103@users.noreply.github.com>
2022-07-13 10:52:53 -0500

    Inital commit >> developing UI elements. log in page (username, password) and logic to drive it.

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.properties import StringProperty
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
 from kivy.uix.relativelayout import RelativeLayout
+# Builder.load_file("login.kv")
 
 
 class MainWidget(GridLayout):
     header_msg = StringProperty("Test Test")
+    username_input = StringProperty("Enter Your Username")
+    password_input = StringProperty("Enter Your Password")
+    submit_enabled = False
+    login_credentials = {"username": "", "password": ""}
+
+
+    def __init__(self, **kwargs):
+        super(MainWidget, self).__init__(**kwargs)
+
+    def clear_username(self):
+        self.username_input = ""
+
+    def clear_password(self):
+        self.password_input = ""
+
+    def check_username(self):
+        if not self.username_input:
+            self.username_input = "Enter your username"
+
+    def validate_password(self, widget):
+        self.login_credentials.update({"password": widget.user_password.text})
 
 
 class TaskApp(App):
