8cfc8e2938a056324b4c325f024ab6ed8e3685e0
Willmo103 <88165849+Willmo103@users.noreply.github.com>
2022-07-14 04:19:47 -0500

    commit >>> basic logic for user validation login. laying out the screens.

--- helpers.py
+++ helpers.py
@@ -1,14 +1,67 @@
 import json
+import os
 
 
-def save_user(func):
-    def wrapper(*args, **kwargs):
-        user_data = func(*args, **kwargs)
-        with open("data/userinfo.json", "w") as f:
-            json.dump(user_data, f)
-            return
+def read_json() -> dict:
+    with open("data/user_info.json", "r") as f:
+        data: dict = json.load(f)
+        return data
 
 
-new_user = {
-    "name": "bill"
-}
+def write_json(data: dict):
+    with open("data/user_info.json", "w") as f:
+        json.dump(data, f)
+
+
+def is_users():
+    data: dict = read_json()
+    users: list = data.get('users')
+    if len(users) == 0:
+        return False
+    else:
+        return True
+
+
+def init_json(self):
+    """
+    This is the initialization process for the startup of a new
+    installation of this application. it will setup the json database and
+    structure to allow for multiple users on the same device. the structure will be:
+    { users: [{
+        "username": "username",
+        "password": "password",
+        "user_tasks": [
+            {task1}, {task2}, {task3},
+            ],
+        "user_daily_tasks": [
+            {task1}, {task2}, {task3},
+            ],
+        "user_tasks_completed": [
+            {task1}, {task2}, {task3},
+            ],
+        "user_points": points,
+        }, etc...]
+    """
+    try:
+        read_json()
+    except FileNotFoundError:
+        if not os.path.exists("data"):
+            os.mkdir("data")
+        json_structure = {
+            "users": []
+        }
+        write_json(json_structure)
+    users = read_json()
+    if len(users.get('users')) == 0:
+        self.state_no_users = True
+    else:
+        self.state_no_users = False
+        data = read_json()
+        users = data.get("users")
+        for user in users:
+            name = user.get("username")
+            password = user.get("password")
+            self.stored_users.append({"username": name, "password": password})
+        # print(self.stored_users)
+
+
--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, BooleanProperty, ObjectProperty
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+sm = ScreenManager()
+
 # Builder.load_file("login.kv")
 
 
 class MainWidget(GridLayout):
-    header_msg = StringProperty("Test Test")
+    from helpers import read_json, write_json, is_users, init_json
+
+    login_widget = ObjectProperty()
+
     username_input = StringProperty("Enter Your Username")
     password_input = StringProperty("Enter Your Password")
+    stored_users: list = [dict, {str: any}]
+    state_no_users = True
     submit_enabled = False
-    login_credentials = {"username": "", "password": ""}
-
+    header_msg = StringProperty("Test Test")
+    state_valid_user = False
 
     def __init__(self, **kwargs):
+        self.init_json()
+        self.new_user_entry()
         super(MainWidget, self).__init__(**kwargs)
 
     def clear_username(self):
@@ -24,12 +36,19 @@ class MainWidget(GridLayout):
     def clear_password(self):
         self.password_input = ""
 
-    def check_username(self):
-        if not self.username_input:
-            self.username_input = "Enter your username"
+    def new_user_entry(self):
+        if self.state_no_users:
+            self.username_input = "Enter your new username"
+            self.password_input = "Enter your new password"
 
-    def validate_password(self, widget):
-        self.login_credentials.update({"password": widget.user_password.text})
+    def validate(self, password, username):
+        if not self.state_no_users:
+            for i in range(2, len(self.stored_users)):
+                current_username = self.stored_users[i].get("username").lower()
+                current_password = self.stored_users[i].get("password")
+                if str(current_password) == password and current_username == username.lower():
+                    self.state_valid_user = True
+                    print(self.state_valid_user)
 
 
 class TaskApp(App):
@@ -37,4 +56,3 @@ class TaskApp(App):
 
 
 TaskApp().run()
-
